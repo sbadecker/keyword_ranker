187e60c9d3a2f97b1615ce2ea1fdf7f517fab8fa
sbadecker <sba.decker@gmail.com>
2017-05-23 12:21:32 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='keyword_ranker',
+      version='0.1',
+      description='Python implementation ranking keywords from a corpus with' +
+      ' with respect to other text files using the Rapid Automatic Keyword' +
+      ' Exctraction algorithm.',
+      url='https://github.com/sbadecker/keyword_ranker',
+      author='Stefan Decker',
+      author_email='sba.decker@gmail.com',
+      license='MIT',
+      packages=['keyword_ranker'],
+      keywords='nlp text-mining algorithms',
+      zip_safe=False)
